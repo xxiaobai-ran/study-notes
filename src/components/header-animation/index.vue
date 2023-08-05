38dd8000e289a3ee91335d3ac7e6ea1924746687
xiongcanyang <849140712@qq.com>
2023-08-05 19:16:24 +0800

    add header

---
+++ src/components/header-animation/index.vue
@@ -0,0 +1,256 @@
+<template>
+  <div class="header__banner">
+    <!---->
+    <div class="animated-banner">
+      <div class="layer">
+        <img
+          src="@/assets/img/header/1.webp"
+          data-height="281"
+          data-width="3000"
+          height="182"
+          width="1950"
+          style="
+            height: 182.65px;
+            width: 1950px;
+            transform: translate(0px, 0px) rotate(0deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/2.webp"
+          data-height="122"
+          data-width="225"
+          height="85"
+          width="157"
+          style="
+            height: 85.4px;
+            width: 157.5px;
+            transform: translate(-280px, 35px) rotate(0deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/3.webp"
+          data-height="218"
+          data-width="816"
+          height="152"
+          width="571"
+          style="
+            height: 152.6px;
+            width: 571.2px;
+            transform: translate(-350px, 0px) rotate(0deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/4.webp"
+          data-height="115"
+          data-width="610"
+          height="86"
+          width="457"
+          style="
+            height: 86.25px;
+            width: 457.5px;
+            transform: translate(-225px, -15px) rotate(5deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/5.webp"
+          data-height="281"
+          data-width="1928"
+          height="210"
+          width="1446"
+          style="
+            height: 210.75px;
+            width: 1446px;
+            transform: translate(75px, -7.5px) rotate(0deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/6.webp"
+          data-height="219"
+          data-width="449"
+          height="153"
+          width="314"
+          style="
+            height: 153.3px;
+            width: 314.3px;
+            transform: translate(-175px, 0px) rotate(0deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/7.webp"
+          data-height="145"
+          data-width="211"
+          height="108"
+          width="158"
+          style="
+            height: 108.75px;
+            width: 158.25px;
+            transform: translate(225px, 0px) rotate(0deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/8.webp"
+          data-height="281"
+          data-width="2459"
+          height="196"
+          width="1721"
+          style="
+            height: 196.7px;
+            width: 1721.3px;
+            transform: translate(-245px, 0px) rotate(0deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/9.webp"
+          data-height="225"
+          data-width="653"
+          height="157"
+          width="457"
+          style="
+            height: 157.5px;
+            width: 457.1px;
+            transform: translate(10.5px, 10.5px) rotate(0deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/10.webp"
+          data-height="181"
+          data-width="524"
+          height="144"
+          width="419"
+          style="
+            height: 144.8px;
+            width: 419.2px;
+            transform: translate(-120px, 0px) rotate(0deg) scale(1);
+            opacity: 0;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/11.webp"
+          data-height="281"
+          data-width="1167"
+          height="196"
+          width="816"
+          style="
+            height: 196.7px;
+            width: 816.9px;
+            transform: translate(0px, 21px) rotate(0deg) scale(1);
+            filter: blur(1px);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/12.webp"
+          data-height="228"
+          data-width="893"
+          height="164"
+          width="642"
+          style="
+            height: 164.16px;
+            width: 642.96px;
+            transform: translate(324px, 0px) rotate(0deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/13.webp"
+          data-height="258"
+          data-width="416"
+          height="185"
+          width="299"
+          style="
+            height: 185.76px;
+            width: 299.52px;
+            transform: translate(381.6px, 0px) rotate(0deg) scale(1);
+            filter: blur(2px);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/14.webp"
+          data-height="281"
+          data-width="2842"
+          height="210"
+          width="2131"
+          style="
+            height: 210.75px;
+            width: 2131.5px;
+            transform: translate(0px, 0px) rotate(0deg) scale(1);
+            filter: blur(1px);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/15.webp"
+          data-height="281"
+          data-width="2257"
+          height="224"
+          width="1805"
+          style="
+            height: 224.8px;
+            width: 1805.6px;
+            transform: translate(0px, 0px) rotate(0deg) scale(1);
+            filter: blur(3px);
+            opacity: 1;
+          "
+        />
+      </div>
+      <div class="layer">
+        <img
+          src="@/assets/img/header/16.webp"
+          data-height="281"
+          data-width="3000"
+          height="224"
+          width="2400"
+          style="
+            height: 224.8px;
+            width: 2400px;
+            transform: translate(0px, 0px) rotate(0deg) scale(1);
+            opacity: 1;
+          "
+        />
+      </div>
+    </div>
+    <!---->
+  </div>
+</template>
+
+<script setup lang="ts">
+import './index.less'
+</script>
